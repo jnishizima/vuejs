d56bb125038593a829b614ea15a6e11c144f2532
Juliano <j.nishizima@hotmail.com>
2022-07-05 10:51:41 -0300

    exemplo do filters usando para criar mascaras de cpf

--- filtros-mixins/src/App.vue
+++ filtros-mixins/src/App.vue
@@ -1,12 +1,27 @@
 <template>
 	<div id="app">
 		<h1>Filtros & Mixins</h1>
+		<hr>
+		<p>{{ cpf | mascaraCpf }}</p>
 	</div>
 </template>
 
 <script>
 export default {
-
+	data(){
+		return{
+			cpf: '60070080090'
+		}
+	},
+	filters: {
+		mascaraCpf(valor){
+			const arr = valor.split('')
+			arr.splice(3,0,'.')
+			arr.splice(7,0,'.')
+			arr.splice(11,0,'-')
+			return arr.join('')
+		}
+	}
 }
 </script>
 
